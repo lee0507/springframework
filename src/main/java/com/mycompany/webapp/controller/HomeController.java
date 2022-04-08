dc90dcf381132f0a1dc793cb75287e719061de6f
lee0507 <2eastbow@gmail.com>
2022-04-08 15:49:17 +0900

    ViewResolver를 ch01_viewresolver.xml로 설정

--- src/main/java/com/mycompany/webapp/controller/HomeController.java
+++ src/main/java/com/mycompany/webapp/controller/HomeController.java
@@ -11,31 +11,15 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
-/**
- * Handles requests for the application home page.
- */
 @Controller
 public class HomeController {
 	
 	private static final Logger logger = LoggerFactory.getLogger(HomeController.class);
 	
-	public HomeController() {
-		logger.info("실행");
-	}
-	/**
-	 * Simply selects the home view to render by returning its name.
-	 */
-	@RequestMapping(value = "/", method = RequestMethod.GET)
+	@RequestMapping("/")
 	public String home(Locale locale, Model model) {
-		logger.info("Welcome home! The client locale is {}.", locale);
-		
-		Date date = new Date();
-		DateFormat dateFormat = DateFormat.getDateTimeInstance(DateFormat.LONG, DateFormat.LONG, locale);
-		
-		String formattedDate = dateFormat.format(date);
-		
-		model.addAttribute("serverTime", formattedDate );
-		
+		logger.info("실행");
+//		return "/WEB-INF/views/home.jsp";
 		return "home";
 	}
 	
